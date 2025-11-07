fb06c3b1355e8b51d8d76927f00f6c9378fbf6fa
Aaditya Bhilegaonkar <aadityab134@gmail.com>
2025-11-07 14:21:24 -0700

    treeeess

--- making_trees.py
+++ making_trees.py
@@ -76,12 +76,12 @@ class Traversals:
                 stack.append(current.left)
         return result
     
-root = Tree(12)
-root.insert(5)
-root.insert(15)
-root.insert(20)
-root.insert(1)
+root = Tree(1)
 root.insert(2)
+root.insert(2)
+root.insert(3)
+root.insert(4)
+root.insert(4)
 root.insert(3)
 
 traverse = Traversals()
